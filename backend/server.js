3c330d725a15e230d129e450ad881c89f999988e
VairaSimone <158765247+VairaSimone@users.noreply.github.com>
2024-09-05 22:47:18 +0200

    sd

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import googleStrategy from './config/passport.config.js';
 const port = process.env.PORT || 5000;
 
 const app = express();
-
+/*
 const corsOptions = {
     origin: (origin, callback) => {
         const whitelist = [`${process.env.FRONTEND_URL}`]; 
@@ -31,6 +31,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+*/
+app.use(cors);
+
 
 app.use(express.json());
 app.use(morgan("dev"))
